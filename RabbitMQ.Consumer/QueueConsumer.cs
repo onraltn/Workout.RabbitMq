08a34cd540182214d648e1abefd1041eff2d2177
onur94altun <onur94altun@gmail.com>
2022-08-16 22:26:53 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/QueueConsumer.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ.Consumer
+{
+    public class QueueConsumer
+    {
+        public static void Consume(IModel channel)
+        {
+            channel.QueueDeclare("demo-queue",
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+            );
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, args) =>
+            {
+                var body = args.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+
+            };
+
+            channel.BasicConsume("demo-queue", true, consumer);
+            Console.WriteLine("Consumer Started...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.Producer/Program.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Producer;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory()
+        {
+            Uri = new Uri("amqp://guest:guest@localhost:5672")
+        };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        QueueProducer.Publish(channel);
+
+    }
+}
